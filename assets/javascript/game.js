138a9f816dc66a7d5c3627b3a705f995f14aae1a
Erin <erinlbritton@gmail.com>
2018-02-21 21:33:23 -0500

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,103 @@
+
+$(document).ready(function() {
+var game = {
+                nums: [1, 2, 3, 4, 5, 6, 7, 8],
+                grid: { 
+                        row1: {
+                                col1: {
+                                        id: 'r1c1', 
+                                        val: 0
+                                    },
+                                col2: {
+                                        id: 'r1c2', 
+                                        val: 0
+                                    },
+                                col3: {
+                                        id: 'r1c3', 
+                                        val: 0
+                                    },
+                                col4: {
+                                        id: 'r1c4', 
+                                        val: 0
+                                    },
+                                
+                            },
+                        row2: {
+                                col1: {
+                                        id: 'r2c1', 
+                                        val: 0
+                                    },
+                                col2: {
+                                        id: 'r2c2', 
+                                        val: 0
+                                    },
+                                col3: {
+                                        id: 'r2c3', 
+                                        val: 0
+                                    },
+                                col4: {
+                                        id: 'r2c4', 
+                                        val: 0
+                                    },
+                            },
+                        row3: {
+                                col1: {
+                                        id: 'r3c1', 
+                                        val: 0
+                                        },
+                                col2: {
+                                        id: 'r3c2', 
+                                        val: 0
+                                        },
+                                col3: {
+                                        id: 'r3c3', 
+                                        val: 0
+                                    },
+                                col4: {
+                                        id: 'r3c4', 
+                                        val: 0
+                                    },
+                            },
+                        row4: {
+                                col1: {
+                                        id: 'r4c1', 
+                                        val: 0
+                                        },
+                                col2: {
+                                        id: 'r4c2', 
+                                        val: 0
+                                        },
+                                col3: {
+                                        id: 'r4c3', 
+                                        val: 0
+                                    },
+                                col4: {
+                                        id: 'r4c4', 
+                                        val: 0
+                                    },
+                            }
+                                
+                                                
+                    },
+                clickCount: 0,
+                getRandomSpot: function(){
+                    //Function to randomly place the number of the array
+                },
+                newGame: function(){
+
+                },
+                checkMatch: function(){
+                    
+                },
+                checkWin: function(){
+
+                },
+                showNumber: function(){
+
+                },
+
+
+
+            }
+
+});
